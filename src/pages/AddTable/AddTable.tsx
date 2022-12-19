0f95a8a649b1b163b650d115e69abba290100c72
yocosaka <sakayoco55@gmail.com>
2022-12-19 09:52:40 +0900

    Add TableInfo component

--- src/pages/AddTable/AddTable.tsx
+++ src/pages/AddTable/AddTable.tsx
@@ -1,11 +1,11 @@
 /*global google*/
 import React, { useState } from 'react';
-import GoogleIcon from '@mui/icons-material/Google';
-import WebAssetIcon from '@mui/icons-material/WebAsset';
+
 import { GoogleMap, StandaloneSearchBox, Marker } from '@react-google-maps/api';
 // import BasicModal from 'src/components/BasicModal';
 import Button from 'src/components/Button';
 import Page from 'src/components/Page';
+import TableInfo from 'src/components/TableInfo';
 import { v4 as uuid } from 'uuid';
 import styles from './AddTable.module.scss';
 import AddTableForm from '../../components/AddTableForm';
@@ -100,53 +100,7 @@ const AddTable = () => {
           </GoogleMap>
         </div>
 
-        <div className={styles.textContainer}>
-          {table && (
-            <>
-              <div className={styles.titleWrapper}>
-                <img
-                  src={table.icon}
-                  alt={table.types ? table.types[0] : table.name}
-                />
-                <h2>{table.name}</h2>
-              </div>
-              {table.photos && (
-                <div className={styles.imageContainer}>
-                  <img
-                    src={table.photos[0].getUrl()}
-                    alt={table.name}
-                    className={styles.mainImage}
-                  />
-                </div>
-              )}
-              <div className={styles.address}>{table.formatted_address}</div>
-              {/* <div>{table.formatted_phone_number}</div> */}
-              {/* <div>Status:{table.business_status}</div> */}
-              {/* <div>Price: {table.price_level}</div> */}
-              <div className={styles.types}>
-                {table.types?.map((type) => (
-                  <div className={styles.type}>{type}</div>
-                ))}
-              </div>
-              {table.url && (
-                <div className={styles.url}>
-                  <GoogleIcon />
-                  <a href={table.url} target="_blank">
-                    See on Google Map
-                  </a>
-                </div>
-              )}
-              {table.website && (
-                <div className={styles.website}>
-                  <WebAssetIcon />
-                  <a href={table.website} target="_blank">
-                    Website
-                  </a>
-                </div>
-              )}
-            </>
-          )}
-        </div>
+        {table && <TableInfo table={table} />}
       </div>
 
       {/* <BasicModal Component={AddTableForm} /> */}
